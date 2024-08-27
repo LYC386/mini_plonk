b236eb9ad04ffe2ee0d348c1f687781bebd27a0f
LYC386 <alex98605@gmail.com>
2024-08-27 16:30:58 +0800

    lagrange interpolation

--- src/poly_util.rs
+++ src/poly_util.rs
@@ -90,6 +90,7 @@ pub fn div<E: Field>(n: &[E], d: &[E]) -> Result<(Vec<E>, Vec<E>), String> {
     }
     Ok((q, r))
 }
+
 pub fn is_zero<E: Field>(f: &[E]) -> bool {
     for i in f {
         if *i != E::ZERO {
@@ -97,6 +98,24 @@ pub fn is_zero<E: Field>(f: &[E]) -> bool {
         }
     }
     true
+}
+
+// interpolate a function with input points (x,y) with Lagrange interpolation
+pub fn interpolate<E: Field>(points: &[(E, E)]) -> Vec<E> {
+    let mut result = vec![E::ZERO];
+    for i in 0..points.len() {
+        let mut l_i = vec![E::ONE];
+        for j in 0..points.len() {
+            if i == j {
+                continue;
+            }
+            let d = points[i].0 - points[j].0;
+            let l_i_j = &[-points[j].0 / d, E::ONE / d];
+            l_i = mul(&l_i, l_i_j);
+        }
+        result = add(&result, &mul(&[points[i].1], &l_i));
+    }
+    return result;
 }
 
 #[cfg(test)]
@@ -125,6 +144,13 @@ mod test {
 
         let r = mul(&m, &n);
         assert_eq!(r, vec![Fq::from(8), Fq::from(10), Fq::from(7), Fq::from(2)]);
+
+        let k = vec![Fq::from(2)];
+        let r2 = mul(&r, &k);
+        assert_eq!(
+            r2,
+            vec![Fq::from(16), Fq::from(20), Fq::from(14), Fq::from(4)]
+        );
     }
 
     #[test]
@@ -170,5 +196,18 @@ mod test {
         let (q, r) = div(&n, &d).unwrap();
         assert_eq!(q, vec![Fq::from(0)]);
         assert_eq!(r, vec![Fq::from(2), Fq::from(1)]);
+    }
+
+    #[test]
+
+    fn test_interpolate() {
+        //f = 2x^2 + 3x + 4
+        let points = &[
+            (Fq::from(0), Fq::from(4)),
+            (Fq::from(1), Fq::from(9)),
+            (Fq::from(3), Fq::from(31)),
+        ];
+        let res = interpolate(points);
+        assert_eq!(res, vec![Fq::from(4), Fq::from(3), Fq::from(2)]);
     }
 }
